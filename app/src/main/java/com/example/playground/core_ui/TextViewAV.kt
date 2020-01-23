2e46825b52e7eef3dafc1d5be91bf7247ac9fe04
Beny Liantriana <benyliantriana@beny.local>
2020-01-23 13:11:10 +0700

    test textView

---
+++ app/src/main/java/com/example/playground/core_ui/TextViewAV.kt
@@ -0,0 +1,22 @@
+package com.example.playground.core_ui
+
+import android.content.Context
+import android.view.Gravity
+import android.view.View
+import androidx.appcompat.widget.AppCompatTextView
+import com.example.playground.R
+
+class TextViewAV(context: Context) : View(context) {
+
+    private var color: Int = context.resources.getColor(R.color.colorAccent)
+
+    val textViewAV = AppCompatTextView(context).apply {
+        textSize = 14f
+        gravity = Gravity.CENTER_HORIZONTAL
+        setTextColor(color)
+    }
+
+    fun getView() : View {
+        return textViewAV.rootView
+    }
+}
--- app/src/main/java/com/example/playground/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/playground/ui/home/HomeFragment.kt
@@ -6,14 +6,61 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.playground.R
+import com.example.playground.core_ui.TextViewAV
+import kotlinx.android.synthetic.main.recycle_view.*
 
 class HomeFragment : Fragment() {
+
+    private var viewList: ArrayList<View> = arrayListOf()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.recycle_view, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        addTextView1()
+        addTextView2()
+        addTextView3()
+        addTextView4()
+        renderView()
+    }
+
+    private fun addTextView1() {
+        viewList.add(TextViewAV(requireContext()).apply {
+            textViewAV.text = "textView 1"
+            textViewAV.setTextColor(resources.getColor(R.color.colorWhite))
+        }.getView())
+    }
+
+    private fun addTextView2() {
+        viewList.add(TextViewAV(requireContext()).apply {
+            textViewAV.text = "textView 2"
+            textViewAV.setTextColor(resources.getColor(R.color.colorWhite))
+        }.getView())
+    }
+
+    private fun addTextView3() {
+        viewList.add(TextViewAV(requireContext()).apply {
+            textViewAV.text = "textView 3"
+            textViewAV.setTextColor(resources.getColor(R.color.colorWhite))
+        }.getView())
+    }
+
+    private fun addTextView4() {
+        viewList.add(TextViewAV(requireContext()).apply {
+            textViewAV.text = "textView 4"
+            textViewAV.setTextColor(resources.getColor(R.color.colorWhite))
+        }.getView())
+    }
+
+    private fun renderView() {
+        viewList.map {
+            linear_layout.addView(it)
+        }
     }
 
 }
